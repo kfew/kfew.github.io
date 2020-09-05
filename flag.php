e0d052fcb9280e25cf752ff56b45181a76e39c57
kfew <1830876746@qq.com>
2020-09-05 20:38:18 +0800

    Site updated: 2020-09-05 20:38:18

---
+++ flag.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<SCRIPT LANGUAGE="Javascript">
+<!--
+
+
+var Words ="%22%3Cscript%3Ewindow.location.href=%27http:%2f%2fwww.baidu.com%27;%3C%2fscript%3E%0A%3C!--Look%20flag%20is%20here!%20--%3E%0A%3C!--ZmxhZ3tzb19pbnN0ZXJlaW5nX3JpZ2h0P30=--%3E" 
+function OutWord()
+{
+var NewWords;
+NewWords = unescape(Words);
+document.write(NewWords);
+} 
+OutWord();
+// -->
+</SCRIPT>
+</HEAD>
+<BODY>
+</BODY>
+</HTML>
